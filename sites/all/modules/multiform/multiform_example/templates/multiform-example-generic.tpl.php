d1ae698d0aabf8f9a171c9e4b0b676729260bdb9
Randall Puterbaugh <rputerbaugh@gmail.com>
2016-09-15 17:03:28 -0700

    Purging

--- sites/all/modules/multiform/multiform_example/templates/multiform-example-generic.tpl.php
+++
@@ -1,9 +0,0 @@
-<p>
-<?php
-print t('You can define your own templates for multiforms. <br />See multiform settings and multifrom_example_theme() for details.');
-?>
-</p>
-
-
-<?php
-print drupal_render_children($form);
